1c30eeb3e9f261bf72c78a69ae010660af25dd66
Abhibykat Singh <nik7273singh@gmail.com>
2026-07-06 19:23:07 +0530

    build own Hooks

--- counter-app/src/Hooks/useCurrencyInfo.js
+++ counter-app/src/Hooks/useCurrencyInfo.js
@@ -1,17 +1,22 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
 
-    useEffect(() => {
-        fetch(
-            `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
-        )
-            .then((res) => res.json())
-            .then((res) => setData(res[currency]));
-    }, [currency]);
+  useEffect(() => {
+    if (!currency) return;
 
-    return data;
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res[currency]);
+      })
+      .catch((err) => console.log(err));
+  }, [currency]);
+
+  return data;
 }
 
 export default useCurrencyInfo;
