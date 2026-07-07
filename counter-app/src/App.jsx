6eb138c856d3deb52f236516bb0867663fb1121e
Abhibykat Singh <nik7273singh@gmail.com>
2026-07-07 13:56:43 +0530

    pass the component values using props

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -4,6 +4,7 @@ import Counter from './components/Counter'
 import CurrencyConverter from './components/CurrencyConverter'
 import PasswordGenerator from './components/PasswordGenerator'
 import TimerUsingHooks from './components/timerUsinghooks'
+import Card from './components/Card'
 
 function App() {
 
@@ -15,7 +16,18 @@ function App() {
 <PasswordGenerator/>
 
 <CurrencyConverter/>
-  
+<Card 
+imgUrl="https://images.pexels.com/photos/36967061/pexels-photo-36967061.jpeg"
+userName="NIkhil"
+age={22}
+Phn={7800468307}/>  
+
+<Card 
+imgUrl="https://images.pexels.com/photos/6259470/pexels-photo-6259470.jpeg"
+userName="Aditya"
+age={24}
+Phn={99191585525}
+/>
   </>
 
  
